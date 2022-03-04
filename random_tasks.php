60ed8f47f265a1eaf9976092f438d116bdda1483
moitran <moitran92@gmail.com>
2022-03-04 17:15:23 +0700

    Revert "format code convention & add .gitignore file"
    This reverts commit 0f9cac22717812f59ff8803e6ebbbc739de67be8.

--- random_tasks.php
+++ random_tasks.php
@@ -8,13 +8,13 @@ $list_person_in_charge = [
     "MINH-KHUE",
     "MINH-NGUYET",
     "LAN-ANH",
-    "TINH-THANH"
+    "TINH-THANH",
 ];
 
 shuffle($list_person_in_charge);
 
 echo "\nOutput: \n";
 foreach ($list_person_in_charge as $task_number => $person_in_charge) {
-    echo sprintf("\tTask-0%s-%s;\n", $task_number + 1, $person_in_charge);
+    echo sprintf("\tTask-0%s-%s\n", $task_number + 1, $person_in_charge);
 }
 echo "\n";
