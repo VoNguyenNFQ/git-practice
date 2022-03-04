d72b5a8ddce54dbd30a6e5ec39301228f024e62a
moitran <moitran92@gmail.com>
2022-03-04 17:15:44 +0700

    Revert "Revert "Update random task #4""
    This reverts commit 2311e80e8b29df4a49c2c45c873ac30b49d6b814.

--- random_tasks.php
+++ random_tasks.php
@@ -15,6 +15,6 @@ shuffle($list_person_in_charge);
 
 echo "\nOutput: \n";
 foreach ($list_person_in_charge as $task_number => $person_in_charge) {
-    echo sprintf("\t0%s-%s\n", $task_number + 1, $person_in_charge);
+    echo sprintf("\t00%s-%s\n", $task_number + 1, $person_in_charge);
 }
 echo "\n";
